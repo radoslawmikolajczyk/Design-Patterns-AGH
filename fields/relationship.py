a08245ca1a85c1951b0d19a4e8332ae6525e1767
JakPopielarz <popielarzjakub7@gmail.com>
2020-12-12 10:51:23 +0100

    Add relationships and PrimaryKey

---
+++ fields/relationship.py
@@ -0,0 +1,27 @@
+from field import Field
+
+
+class Relationship(Field):
+    """
+        A class to represent a relationship between database tables
+
+        Attributes
+        -----------------
+        other : OneToOne / OneToMany / ManyToMany object
+        name : String
+    """
+    def __init__(self, other, name="DEFAULT"):
+        self.other = other
+        self.name = name
+
+class OneToOne(Relationship):
+    def __init__(self, other, name):
+        super().__init__(other, name)
+
+class OneToMany(Relationship):
+    def __init__(self, other, name):
+        super().__init__(other, name)
+
+class ManyToMany(Relationship):
+    def __init__(self, other, name):
+        super().__init__(other, name)
