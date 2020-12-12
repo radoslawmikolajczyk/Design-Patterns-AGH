2fb65a926e407a58cb4d3536b4c1683932396bec
JakPopielarz <popielarzjakub7@gmail.com>
2020-12-12 11:05:04 +0100

    Added Entity class

---
+++ entity/entity.py
@@ -0,0 +1,15 @@
+class Entity:
+    """
+        A class to represent the database table entity
+        
+        Attributes
+        -----------------
+        name : String
+
+        Methods
+        -----------------
+        __init__(self, name):
+            initializes object with the given attributes
+    """
+    def __init__(self, name):
+        self._table_name = name
