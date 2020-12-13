fb941d556ce79c3d5869b849ea948ed90e512788
Mateusz Jachimski <mateusz.jachimski@gmail.com>
2020-12-13 04:35:55 +0100

    Create builder string utils

---
+++ builders/test_stringutils.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+import builders.stringutils as strutl
+import re
+
+
+class TestStringUtils(TestCase):
+
+    def test_quote_database_name_unsafe(self):
+        self.assertEqual(strutl.quote_database_object_name_unsafe('ABCD"GHD'), '"ABCD""GHD"')
+
+    def test_quote_known_string_unsafe(self):
+        self.assertEqual(strutl.quote_known_string_unsafe("ABCD'GHD"), "'ABCD''GHD'",)
+
+    def test_quote_string(self):
+        self.assertRegex(strutl.quote_string("ABCD"), r"\$[A-Za-z0-9]{5}\$ABCD\$[A-Za-z0-9]{5}\$")
